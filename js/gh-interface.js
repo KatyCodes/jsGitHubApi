80ebb879a62db3f7dfaa835d42a76dd5bcbea695
Katy Henning <katyannhenning@gmail.com>
2016-10-14 09:21:53 -0700

    add initial api connection

--- js/gh-interface.js
+++ js/gh-interface.js
@@ -1,6 +1,10 @@
-
-
 $(document).ready(function() {
-
-  
+  $('#text').click(function(){
+    $.get('https://api.github.com/users/katycodes/repos?access_token=247db9e4c67f7c7efeeb20d5fe4997258bf18bef').then(function(response){
+      console.log(response);
+      $('#text').html("this is working");
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+});
 });
