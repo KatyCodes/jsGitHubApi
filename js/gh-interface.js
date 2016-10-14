a034ddf7ffc535801b12ad264a6b47e10c98b0d5
Katy Henning <katyannhenning@gmail.com>
2016-10-14 10:41:15 -0700

    seperates backend from UI

--- js/gh-interface.js
+++ js/gh-interface.js
@@ -1,15 +1,11 @@
 var apiKey = require('./../.env').apiKey;
+var GitHub = require('./../js/gh.js').githubModule;
+
 
 $(document).ready(function() {
   $('#button').click(function(event){
     event.preventDefault();
-    var userInput = $('#userInput').val().toLowerCase();
-    $.get('https://api.github.com/users/'+ userInput + '/repos?access_token='+ apiKey).then(function(response){
-      console.log(response);
-      for (i = 0; i < response.length; i++) {
-            $('ol').append('<li>' + response[i].full_name + '</li>'); }
-    }).fail(function(error){
-      console.log(error.responseJSON.message);
-    });
-});
+    var currentUser = new GitHub();
+    currentUser.getUser();
+  });
 });
---
+++ js/gh.js
@@ -0,0 +1,16 @@
+var apiKey = require('./../.env').apiKey;
+
+function GitHub(){
+
+}
+GitHub.prototype.getUser = function(){
+  var userInput = $('#userInput').val();
+  $.get('https://api.github.com/users/'+ userInput + '/repos?access_token='+ apiKey).then(function(response){
+    console.log(response);
+    for (i = 0; i < response.length; i++) {
+          $('ol').append('<hr><li> Name: ' + response[i].full_name + '</li>' + 'Description: ' + response[i].description); }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  }
+exports.githubModule = GitHub;
