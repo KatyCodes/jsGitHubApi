b8a7c428e781ff6739f9abc5992fbb135d05b15f
Katy Henning <katyannhenning@gmail.com>
2016-10-14 09:56:41 -0700

    add initial javascript code to show github repos

--- js/gh-interface.js
+++ js/gh-interface.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
   $('#text').click(function(){
     $.get('https://api.github.com/users/katycodes/repos?access_token='+ apiKey).then(function(response){
       console.log(response);
-      $('#text').html("this is working");
+      for (i = 0; i < response.length; i++) {
+            $('ul').append('<li>' + response[i].full_name + '</li>'); }
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
