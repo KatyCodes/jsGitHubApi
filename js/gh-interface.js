c9994ca8015aabf44a6fecd855d9030cce757945
Katy Henning <katyannhenning@gmail.com>
2016-10-14 09:34:05 -0700

    add env file to hold api key

--- js/gh-interface.js
+++ js/gh-interface.js
@@ -1,6 +1,8 @@
+var apiKey = require('./../.env').apiKey;
+
 $(document).ready(function() {
   $('#text').click(function(){
-    $.get('https://api.github.com/users/katycodes/repos?access_token=247db9e4c67f7c7efeeb20d5fe4997258bf18bef').then(function(response){
+    $.get('https://api.github.com/users/katycodes/repos?access_token='+ apiKey).then(function(response){
       console.log(response);
       $('#text').html("this is working");
     }).fail(function(error){
