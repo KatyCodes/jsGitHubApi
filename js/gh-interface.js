96871cd742d9ca8ca6449a830c2de00545126237
Katy Henning <katyannhenning@gmail.com>
2016-10-14 10:14:31 -0700

    add user input form and provide userinput to search github

--- js/gh-interface.js
+++ js/gh-interface.js
@@ -1,11 +1,13 @@
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $('#text').click(function(){
-    $.get('https://api.github.com/users/katycodes/repos?access_token='+ apiKey).then(function(response){
+  $('#button').click(function(event){
+    event.preventDefault();
+    var userInput = $('#userInput').val().toLowerCase();
+    $.get('https://api.github.com/users/'+ userInput + '/repos?access_token='+ apiKey).then(function(response){
       console.log(response);
       for (i = 0; i < response.length; i++) {
-            $('ul').append('<li>' + response[i].full_name + '</li>'); }
+            $('ol').append('<li>' + response[i].full_name + '</li>'); }
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
